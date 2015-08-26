4dfd3963c6f3e7df495ee565b208242bdf043c2f
Pedro David <peddavid96@outlook.com>
2015-08-26 12:38:24 +0100

    Revert "Revert "Texture Arrays""
    This reverts commit c11bb71fe8de7cebe5835abc4bed2c30840b879b.

--- NubDevEngine - core/res/shaders/basic.vert
+++ NubDevEngine - core/res/shaders/basic.vert
@@ -2,7 +2,7 @@
 
 layout (location = 0) in vec4 position;
 layout (location = 1) in vec2 uv;
-layout (location = 1) in float tid;
+layout (location = 2) in float tid;
 layout (location = 3) in vec4 color;
 
 uniform mat4 pr_matrix;
--- NubDevEngine - core/src/graphics/shader.h
+++ NubDevEngine - core/src/graphics/shader.h
@@ -21,7 +21,9 @@ namespace engine{
 			~Shader();
 
 			void setUniform1f(const GLchar *name, float value);
+			void setUniform1fv(const GLchar *name, float *value, GLsizei size);
 			void setUniform1i(const GLchar *name, int value);
+			void setUniform1iv(const GLchar *name, int *value, GLsizei size);
 			void setUniform2f(const GLchar *name, const maths::vec2& vector);
 			void setUniform3f(const GLchar *name, const maths::vec3& vector);
 			void setUniform4f(const GLchar *name, const maths::vec4& vector);
